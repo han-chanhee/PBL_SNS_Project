26bab5108b8e2b27f788bc15c850940c95b8dfa5
mzmz1120 <mzmzk.11.20@gamil.com>
2022-11-28 01:55:16 +0900

    자동로그인

--- app/src/main/java/com/example/pbl_sns_project/SplashActivity.kt
+++ app/src/main/java/com/example/pbl_sns_project/SplashActivity.kt
@@ -4,15 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
+    private  lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = Firebase.auth
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
-        }, 2000)
-    }
+        auth = Firebase.auth
+
+        if(auth.currentUser?.uid == null) {
+            Handler().postDelayed({
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }, 2000)
+        } else {
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 2000)
+        }
+
+        }
+
+
 }
